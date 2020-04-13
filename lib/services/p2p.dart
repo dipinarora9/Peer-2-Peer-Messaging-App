b2151b9018470fdd040f47ad2e2525322769b7ea
prashant_wan <pokerosajwan0@gmail.com>
2020-04-13 20:57:36 +0530

    added schema

--- lib/services/p2p.dart
+++ lib/services/p2p.dart
@@ -1,3 +1,21 @@
 import 'package:flutter/foundation.dart';
 
-class P2P with ChangeNotifier {}
+import 'dart:io';
+
+class P2P with ChangeNotifier {
+
+
+  P2P() {
+    // connect to booting peer -> my routing table
+  }
+
+  // ping pong + timer all checking that if the peer is alive
+  // get block push mem pool and
+  // send block
+
+
+ setupTCPServer(){
+//    ServerSocket.bind(address, port);
+
+ }
+}
